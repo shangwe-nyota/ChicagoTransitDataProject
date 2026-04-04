492c6856cf68e23fda0989a5e4528c8e3da37420
Shangwe Nyota <s.nyota@wustl.edu>
2026-04-04 16:18:13 -0500

    Added clean tables ddl

---
+++ sql/ddl/clean_tables.sql
@@ -0,0 +1,41 @@
+CREATE OR REPLACE TABLE CLEAN_GTFS_STOPS (
+    stop_id STRING,
+    stop_name STRING,
+    stop_lat FLOAT,
+    stop_lon FLOAT,
+    location_type INTEGER,
+    parent_station STRING
+);
+
+CREATE OR REPLACE TABLE CLEAN_GTFS_ROUTES (
+    route_id STRING,
+    route_short_name STRING,
+    route_long_name STRING,
+    route_type INTEGER
+);
+
+CREATE OR REPLACE TABLE CLEAN_GTFS_TRIPS (
+    route_id STRING,
+    service_id STRING,
+    trip_id STRING,
+    direction_id INTEGER,
+    shape_id STRING,
+    direction STRING,
+    wheelchair_accessible INTEGER,
+    schd_trip_id STRING
+);
+
+CREATE OR REPLACE TABLE CLEAN_GTFS_STOP_TIMES (
+    trip_id STRING,
+    arrival_time STRING,
+    departure_time STRING,
+    stop_id STRING,
+    stop_sequence INTEGER
+);
+
+CREATE OR REPLACE TABLE CLEAN_GTFS_SHAPES (
+    shape_id STRING,
+    shape_pt_lat FLOAT,
+    shape_pt_lon FLOAT,
+    shape_pt_sequence INTEGER
+);
