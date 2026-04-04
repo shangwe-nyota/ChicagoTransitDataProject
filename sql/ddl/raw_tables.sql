c8905161af9b1447583e238cbcaca92b5e877fa0
Shangwe Nyota <s.nyota@wustl.edu>
2026-04-04 16:20:12 -0500

    Added todo in raw tables ddl

---
+++ sql/ddl/raw_tables.sql
@@ -0,0 +1,3 @@
+-- Raw GTFS tables can be added later if needed. Really not that important since we transformed the data.
+-- For phase 1 of project, we are focusing on clean and analytics tables in Snowflake.
+-- TODO: Later come back and add raw ddl just for source of truth purposes - SHANGWE
